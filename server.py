fd3f8995debcd909218398eee6889784f8fd5e0b
Dogukan <dogukanberkay.ertem@ogr.deu.edu.tr>
2020-08-06 15:22:06 +0300

    Çalışan hale geri döndü
    06.08.2020 çalışan haline geri döndü

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 import socket
-from cv2 import cv2
+import cv2
 import pickle
 import numpy as np
 import struct
 
 HOST = '169.254.43.118' #Bağlantının kurulacağı IPV4 adresi
-PORT = 20203 #Bağlantının sağlanacağı port
+PORT = 20203 #Bağlantının portu
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Bağlantının adres ailesi ve türü
 
@@ -15,28 +15,23 @@ s.listen(10)
 print("Client bekleniyor...")
 
 conn,addr = s.accept() #Conn = bağlantı sağlanıp sağlanamadığı, addr = bağlananın bilgileri
-print(conn)
-print(addr)
+
 data = b"" #Verileri byte cinsinden tutmak için string formatting
 payload_size = struct.calcsize(">L")
-print(payload_size)
-print(data)
+
 while True:
-    
-    print("deurovteam")
     while len(data) < payload_size: #Elimizde gelen veri yoksa
         data += conn.recv(4096)     #veriyi alıyoruz
 
-    packed_msg_size = data[:payload_size]
+    packed_msg_size = data[:payload_size] #Gelen mesajın sıkıştırılmış boyutu
     data = data[payload_size:]            #Gelen mesaj
-    msg_size = struct.unpack(">L", packed_msg_size)[0]
+    msg_size = struct.unpack(">L", packed_msg_size)[0] #Gelen mesajın sıkıştırılmamış boyutu
     while len(data) < msg_size:                        
         data += conn.recv(4096)
     frame_data = data[:msg_size]
-    
+    data = data[msg_size:]
+
     frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes") #Byte cinsinden aldığımız verileri encode edip frame değişkenine attık
-    
     frame = cv2.imdecode(frame, cv2.IMREAD_COLOR) #Frame'i decode ediyoruz
     cv2.imshow("KAMERA", frame) #Ekranda açılacak olan frame'in özellikleri
     cv2.waitKey(1)
-
