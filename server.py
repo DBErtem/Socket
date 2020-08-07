c7b4991ef1503db7a0710ebf6412c1f0d9d892e3
Dogukan <dogukanberkay.ertem@ogr.deu.edu.tr>
2020-08-07 17:01:00 +0300

    Ara sıra da olsa bytearray 1 yolluyor

--- server.py
+++ server.py
@@ -14,21 +14,25 @@ s.listen(10)
 print("Client bekleniyor...")
 
 conn,addr = s.accept()
-
+print(conn)
+print(addr)
 data = b""
 payload_size = struct.calcsize(">L")
 
 while True:
     while len(data) < payload_size:
         data += conn.recv(4096)
-
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
+    
     msg_size = struct.unpack(">L", packed_msg_size)[0]
-    while len(data) < msg_size:                        
+    print("msg: ", msg_size)
+
+    while len(data) < msg_size:
         data += conn.recv(4096)
+    print(data[-4:])
     frame_data = data[:msg_size]
-    data = data[msg_size:]
+    data = data[msg_size:-4]
 
     frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
     frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
