fc33618f4ba1afb5be956516ca48169271efaeb2
Dogukan <dogukanberkay.ertem@ogr.deu.edu.tr>
2020-08-28 16:50:18 +0300

    Client çift taraflı haberleşme çalışıyor

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import imutils
 from imutils.video import VideoStream
 import argparse
 import time
@@ -6,29 +7,31 @@ import struct
 import pickle
 import socket
 
-HOST = '169.254.69.15' #Bağlantının kurulacağı IPV4 adresi
-PORT = 20203 #Bağlantının sağlanacağı port
+HOST = '169.254.69.15'
+PORT = 20203
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Bağlantının adres ailesi ve türü
-client_socket.connect((HOST,PORT)) #Uzaktaki sunucuya bağlanıp SSL ile şifreleyen komut
-connection = client_socket.makefile('wb') 
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST,PORT))
+#connection = client_socket.makefile('wb') 
 
 ap = argparse.ArgumentParser() 
-ap.add_argument("-p", "--picamera", type=int, default=-1, help="Picamera modülünü terminal koduna dönüştürdük")
+ap.add_argument("-p", "--picamera", type=int, default=-1)
 args = vars(ap.parse_args())
 
-vs = VideoStream(usePiCamera=args["picamera"] > 0).start() #Kamera kaydını başlatıyoruz
+vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
 time.sleep(0.2)
 
 while True:
-    frame = vs.read() #Kameradan gelen verileri frame değişkenine atıyoruz
-    frame = imutils.resize(frame, width=800) #Görüntünün genişliğini 800px yapıyoruz
+    frame = vs.read()
+    
 
-    result, frame = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 90])  #Her bir frame'in kalitesini %90 olacak şekilde ayarladık.
+    result, frame = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
     data = pickle.dumps(frame, 0)
     size = len(data)
-    client_socket.sendall(struct.pack(">L", size) + data) #Frame verilerini sunucuya yolluyoruz
+    client_socket.sendall(struct.pack(">L", size) + data)
+    print(client_socket.recv(1024))
     
-cv2.destroyAllWindows() #İşimiz bittikten sonra pencereleri kapatıyoruz
-vs.stop()               #Kamerayı durduruyoruz
+cv2.destroyAllWindows()
+vs.stop()               
+client_socket.close()
 
