7f99c362deae07f8c436dd0e0c1a7df760891a83
Dogukan <dogukanberkay.ertem@ogr.deu.edu.tr>
2020-08-28 16:39:15 +0300

    Server çift taraflı haberleşme çalışıyor

--- server.py
+++ server.py
@@ -5,8 +5,9 @@ import numpy as np
 import struct
 import time
 import imutils
+import random
 
-HOST = '169.254.43.118'
+HOST = '169.254.69.119'
 PORT = 20203
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,24 +21,35 @@ print(conn)
 print(addr)
 data = b""
 payload_size = struct.calcsize(">L")
+
+
 while True:
+    randomsayi = random.randint(5,10)
+    conn.send(bytes([randomsayi]))
+    #print(conn.recv(1024))
+    
     while len(data) < payload_size:
         data += conn.recv(4096)
+        
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
     
     msg_size = struct.unpack(">L", packed_msg_size)[0]
     print("msg:", msg_size)
-
+    
     while len(data) <= msg_size:
         data += conn.recv(4096)
     
     frame_data = data[:msg_size]
     data = data[msg_size:]
     
-    print(data)
+    
+    #print(data)
     frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
     frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-    frame = imutils.resize(frame,width=800)
+    frame = imutils.resize(frame,width=1600)
     cv2.imshow("KAMERA", frame)
+    
     cv2.waitKey(1)
+    
+s.close()
